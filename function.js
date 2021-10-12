306681a702ac8a2f7711e74b8bde53d6f82586e2
Krizha <krishmikaabisheka1@gmail.com>
2021-10-12 20:59:30 +0530

    trying to make it work
    56 th attempt

--- function.js
+++ function.js
@@ -1,12 +1,11 @@
 var convertBtn = document.querySelector('.convert-button');
 var URLinput = document.querySelector('.URL-input');
+
 convertBtn.addEventListener('click', () => {
     console.log(`URL: ${URLinput.value}`);
     sendURL(URLinput.value);
 });
+
 function sendURL(URL) {
-    fetch(`http://devil-prog.github.io:4000/download?URL=${URL}`, {
-        method:'GET'
-    }).then(res => res.json())
-    .then(json => console.log(json));
+    window.location.href = `http://localhost:4000/download?URL=${URL}`;
 }
