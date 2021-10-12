b2da32b0daa989e1efde8dff9e03bc612df79eef
devil-prog <krishmikaabisheka1@gmail.com>
2021-10-12 20:29:59 +0530

    made it better

--- function.js
+++ function.js
@@ -1,11 +1,12 @@
 var convertBtn = document.querySelector('.convert-button');
 var URLinput = document.querySelector('.URL-input');
-
 convertBtn.addEventListener('click', () => {
     console.log(`URL: ${URLinput.value}`);
     sendURL(URLinput.value);
 });
-
 function sendURL(URL) {
-    window.location.href = `https://yt-download1001.herokuapp.com:4000/download?URL=${URL}`;
+    fetch(`http://localhost:4000/download?URL=${URL}`, {
+        method:'GET'
+    }).then(res => res.json())
+    .then(json => console.log(json));
 }
