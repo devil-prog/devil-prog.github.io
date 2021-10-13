8620e337ea78eb3cf0d087da162d81783ea893cb
Krizha <krishmikaabisheka1@gmail.com>
2021-10-14 00:29:47 +0530

    URL filters updated

--- function.js
+++ function.js
@@ -7,11 +7,18 @@ convertBtn.addEventListener('click', () => {
     
     if (URLinput.value.includes("://youtu.be") || URLinput.value.includes("://www.youtube.com") || URLinput.value.includes("://youtube.com")){
         yturl(URLinput.value);
+    } else if (URLinput.value.includes("://www.instagram") || URLinput.value.includes("://www.instagram.com")) {
+        igvid(URLinput.value);
     } else {
-        alert('WRONG URL!')
+        alert("error");
     }
 });
 
 function yturl(URL) {
     window.location.href = `https://writhingminorcones.krishmikaabhish.repl.co/download?URL=${URL}`;
+}
+
+function igvid(URL) {
+    /* window.location.href = `https://writhingminorcones.krishmikaabhish.repl.co/igvid?URL=${URL}`; */
+    alert ("insta video downloader on working!!")
 }
